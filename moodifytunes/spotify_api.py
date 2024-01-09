e0d821dceb5f468d2f75e6bf9c56b7a09c6eb016
weibik <j-szkola@wp.pl>
2024-01-09 20:36:36 +0100

    add 'get code from web' and parameters to data

--- moodifytunes/spotify_api.py
+++ moodifytunes/spotify_api.py
@@ -1,9 +1,10 @@
 import json
 import os
 import base64
-from spotipy.oauth2 import SpotifyOAuth
 from dotenv import load_dotenv
 from requests import post, get
+import webbrowser
+from urllib.parse import urlencode
 
 
 load_dotenv()
@@ -19,13 +20,31 @@ def get_token():
     url = "https://accounts.spotify.com/api/token"
     headers = {
         "Authorization": "Basic " + auth_base64,
-        "Content-Type": "application/x-www-form-urlencoded"
+        "Content-Type": "application/x-www-form-urlencoded",
     }
-    data = {"grant_type": "client_credentials"}
+    data = {
+        "grant_type": "client_credentials",
+        "code": "AQAc1v9YJadkDlaQlLrTIqcpruqfsOitLwEZuB2eRmSMLu2ZT9YjqUo0FgRJgvbI0Z3X4TfDYT9NfQ1zIqQe-iBWTLqZ0Ni8H2wQ"
+                "663-J0g3jXmi4m6cuSvhLq4ZE7wlOJ97hlgrDFbl324iebfp8WXc1ALC7MrBtDGH-IFxhpT1d7_OZauDnsaJKzxM3dSKdZNYT"
+                "9lL7FE-",
+        "redirect_uri": "http://localhost/",
+    }
+
     result = post(url, headers=headers, data=data)
     json_result = json.loads(result.content)
     token = json_result["access_token"]
     return token
+
+
+def get_code_from_web():
+    auth_headers = {
+        "client_id": client_id,
+        "response_type": "code",
+        "redirect_uri": "http://localhost/",
+        "scope": "user-read-email user-read-private",
+    }
+
+    webbrowser.open("https://accounts.spotify.com/authorize?" + urlencode(auth_headers))
 
 
 def get_auth_header(token):
@@ -33,12 +52,13 @@ def get_auth_header(token):
 
 
 def get_user_id(token):
-    """ TODO: Needs user-read-private and user-read-email to have authorization. """
+    """TODO: Needs user-read-private and user-read-email to have authorization."""
 
     url = "https://api.spotify.com/v1/me"
     headers = get_auth_header(token)
     query_url = url
-    result = get(query_url, headers=headers)
+    user_params = {"limit": 50}
+    result = get(query_url, params=user_params, headers=headers)
     json_result = json.loads(result.content)
     if len(json_result) == 0:
         print(f"No playlists with name {playlist_name} exits.")
@@ -52,6 +72,7 @@ def search_for_artist(token, artist_name):
     headers = get_auth_header(token)
     query = f"?q={artist_name}&type=artist&limit=1"
     query_url = url + query
+
     result = get(query_url, headers=headers)
     json_result = json.loads(result.content)["artists"]["items"]
     if len(json_result) == 0:
@@ -81,4 +102,3 @@ def get_songs_by_artist(token, artist_id):
     result = get(url, headers=headers)
     json_result = json.loads(result.content)["tracks"]
     return json_result
-
